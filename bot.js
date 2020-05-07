db4d67abc1f9305a032861a322e8ca11b4dc755c
snkrdog <64150236+snkrdog@users.noreply.github.com>
2020-05-07 01:27:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,76 +1,46 @@
 const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-const client = new Discord.Client();
- 
+const token = 'NzA0MzM2NzkzODcyNjI5Nzkx.Xqbq2g.zjIdNOuqBqcxctvqFMM7LklYUCE';
 
-client.on('ready', () => {
+const prefix = '-';
 
+bot.on('ready', () =>{
     console.log('wavehelp');
+})
 
-});
+bot.on('message', msg=>{
 
- 
+  let args = msg.content.substring(prefix.length).split(" ");
 
-client.on('message', message => {
-
-    if (message.content === 'twitter') {
-
-       message.reply('https://twitter.com/wavequeue%27);
-       
-       }
-
-    if (message.content === 'yeezysupply') {
-
-       message.reply('https://www.yeezysupply.com/%27');
-       
-       }
-
-    if (message.content === 'adidas') {
-
-       message.reply('https://www.adidas.com/us/yeezy%27);
-       
-       }
-
-    if (message.content === 'version') {
-
-       message.reply('1.0.0 | beta launch');
-       
-       }
-
-    if (message.content === 'update') {
-
-       message.reply('update zip link');
-       
-       }
-
-    if (message.content === 'install') {
-
-       message.reply('install zip link');
-       
-       }
-
-    if (message.content === 'guide') {
-
-       message.reply('coming soon');
-       
-       }
-
-    if (message.content === 'help') {
-
-       message.reply('coming soon');
-
-       }       
-    
-    if (message.content === 'release') {
-
-       message.reply('Sulfur | FY5346');
-
-       }       
-       
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);
+  switch(args[0]){
+    case 'twitter':
+      msg.reply('https://twitter.com/wavequeue%27);
+      break;
+    case 'yeezysupply':
+      msg.reply('https://www.yeezysupply.com/%27);
+      break;
+    case 'adidas':
+      msg.reply('https://www.adidas.com/us/yeezy%27);
+      break;
+    case 'version':
+      msg.reply('wavequeue version 1.0.0 | beta launch');
+      break;
+    case 'update':
+      msg.reply('update zip link');
+      break;
+    case 'install':
+      msg.reply('install zip link');
+      break;
+    case 'guide':
+      msg.reply('coming soon');
+      break;
+    case 'help':
+      msg.reply('coming soon');
+      break;
+    case 'release':
+      msg.reply('Sulfur | FY5346%27');
+      break;
+  }
+})
+bot.login(token);
